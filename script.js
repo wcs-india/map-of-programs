45984f16047d5fb30f7b146d93e3b120df21aed0
monsoonforest <chintz604@gmail.com>
2022-10-27 11:04:21 +0530

    first commit

--- script.js
+++ script.js
@@ -37,34 +37,12 @@ new L.esri.basemapLayer('ImageryLabels').addTo(map);
 // Edit to upload GeoJSON data file from your local directory
 $.getJSON("test-program-sites.geojson", function (data) {
  geoJsonLayer = L.geoJson(data, {
-    style: style,
+    style: {color: '#42ff3f', weight:1, fillOpacity: 0},
     onEachFeature: onEachFeature
   }).addTo(map);
 controlLayers.addOverlay(geoJsonLayer, 'Programs');
 
 });
-
-// $.getJSON("places.geojson", function (data) {
-//   geoJsonLayer = L.geoJson(data, {
-//     style: {color: 'white'},
-    
-//   }).addTo(map);
-// controlLayers.addOverlay(geoJsonLayer, 'Metalled Roads');
-// });
-
-
-
-
-// Edit the getColor property to match data column header in your GeoJson file
-function style(feature) {
-  return {
-    fillColor: getColor(feature.properties.Program),
-    weight: 0.8,
-    opacity: 1,
-    color: 'white',
-    fillOpacity: 1
-  };
-}
 
 
 // This highlights the layer on hover, also for mobile
@@ -109,11 +87,11 @@ info.onAdd = function (map) {
 };
 
 // Edit info box text and variables (such as elderly density 2014) to match those in your GeoJSON data
-// info.update = function (props) {
-//   this._div.innerHTML = '<h4>Sagalee Constituency<br />Population of Senior Citizens 2020</h4>' +  (props ?
-//     '<b>' + props.Program + ' ' + props.Site + '</b><br />');
-// };  
+info.update = function (props) {
+  this._div.innerHTML = '<h4>Sagalee Constituency<br />Population of Senior Citizens 2020</h4>' +  (props ?
+    '<b>' + props.Program + ' ' + props.Site + '</b><br />' );
+};  
 
 
-// info.addTo(map);
+info.addTo(map);
 
