1eb1b1ad8d04f4b7d51a1616cbf572d0bc484778
monsoonforest <chintz604@gmail.com>
2022-11-03 12:24:57 +0530

    sidebar-test

--- script.js
+++ script.js
@@ -1,90 +1,186 @@
-// Edit the center point and zoom level
-var map = L.map('map', {
-  center: [17.876475, 77.379697],
-  zoom: 9  ,
-  minZoom : 5,
-  scrollWheelZoom: true
-});
+/* eslint-disable no-undef */
+/**
+ * sidebar replacing popup
+ */
+
+// config map
+let config = {
+  minZoom: 7,
+  maxZoom: 18,
+};
+// magnification with which the map will start
+const zoom = 6;
+// co-ordinates
 
 
-// layer controls
-var controlLayers = L.control.layers( null, null, {
-     position:"topleft",
-     collapsed: false // truw = closed by default
-    }).addTo(map);
+const lat = 77.379697;
+const lng = 20.876475;
 
+// calling map
+const map = L.map("map", config).setView([lat, lng], zoom);
 
+// Used to load and display tile layers on the map
+// Most tile servers require attribution, which you can set under `Layer`
 new L.esri.basemapLayer('Imagery').addTo(map);
 new L.esri.basemapLayer('ImageryLabels').addTo(map);
+// ------------------------------------------------------------
+// async function to get data from json
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+}
 
+// --------------------------------------------------
+// button to close sidebar
+const buttonClose = document.querySelector(".close-button");
 
-// Edit to upload GeoJSON data file from your local directory
-$.getJSON("test-program-sites.geojson", function (data) {
- geoJsonLayer = L.geoJson(data, {
-      style: {
-      color: '#42ff3f', 
-      weight:4, 
-      fillOpacity: 0
-    },
-    onEachFeature: onEachFeature
-  }).addTo(map);
-controlLayers.addOverlay(geoJsonLayer, '<b>PROGRAMS</b>');
+let featureGroups = [];
+let groupBounds;
+let latlngs = [];
 
+// function to add markers to map
+fetchData("great-indian-bustard-sites.geojson")
+  .then((data) => {
+    // create markers width "marker-options-id"
+    data.map((marker) => {
+      featureGroups.push(
+        L.marker(marker.geometry, {
+          icon: L.divIcon({
+            className: "leaflet-marker-icon",
+            html: `${marker.fid}`,
+            iconSize: L.point(30, 30),
+            popupAnchor: [3, -5],
+          }),
+          "marker-options-id": marker.fid,
+        })
+      );
+      latlngs.push(marker.geometry);
+    });
+
+        return data;
+  })
+  .then((data) => {
+    // create feature group
+    // add markers to map
+    featureGroups.map((marker) => {
+      marker.addTo(map);
+    });
+
+    // create feature group with markers
+    groupBounds = new L.featureGroup(featureGroups);
+
+    // fitBounds of feature group to map
+    map.fitBounds(groupBounds.getBounds(), {
+      padding: [50, 50],
+    });
+
+    // add event listener to markers to open sidebar
+    groupBounds.on("click", function (e) {
+      if (e.layer instanceof L.Marker) {
+        showSidebarWidthText(e.layer.options["marker-options-id"]);
+      }
+    });
+
+    // add comment to sodebar depending on marker id
+    function showSidebarWidthText(id) {
+      data.filter((marker) => {
+        if (marker.fid === id) {
+          document.body.classList.add("active-sidebar");
+          addContentToSidebar(marker);
+        }
+      });
+    }
+  });
+
+// --------------------------------------------------
+// close when click esc
+document.addEventListener("keydown", function (event) {
+  // close sidebar when press esc
+  if (event.key === "Escape") {
+    closeSidebar();
+  }
 });
 
+// close sidebar when click on close button
+buttonClose.addEventListener("click", () => {
+  // close sidebar when click on close button
+  closeSidebar();
+});
 
+// close sidebar when click outside
+// document.addEventListener("click", (e) => {
+//   const target = e.target;
+//   if (
+//     !target.closest(".sidebar") &&
+//     !target.classList.contains("leaflet-marker-icon")
+//   ) {
+//     closeSidebar();
+//   }
+// });
 
+// --------------------------------------------------
+// close sidebar
 
-// This highlights the layer on hover, also for mobile
-function highlightFeature(e) {
-  resetHighlight(e);
-  var layer = e.target;
-  layer.setStyle({
-    weight: 4,
-    color: 'red',
-    fillOpacity: 0.2
+function closeSidebar() {
+  // remove class active-sidebar
+  document.body.classList.remove("active-sidebar");
+
+  // bounds map to default
+  boundsMap();
+}
+
+// --------------------------------------------------
+// add content to sidebar
+
+function addContentToSidebar(marker) {
+  const { fid, Program, small, description, img, geometry } = marker;
+  const smallInfo = small !== undefined ? `<small>${small}</small>` : "";
+
+  // create sidebar content
+  const sidebarTemplate = `
+    <article class="sidebar-content">
+      <h1>${Program}</h1>
+      <div class="marker-id">${fid}</div>
+      <div class="info-content">
+        <img class="img-zoom" src="${img}">
+        ${smallInfo}
+        <div class="info-description">${description}</div>
+      </div>
+    </article>
+  `;
+
+  const sidebar = document.querySelector(".sidebar");
+  const sidebarContent = document.querySelector(".sidebar-content");
+
+  // always remove content before adding new one
+  sidebarContent?.remove();
+
+  // add content to sidebar
+  sidebar.insertAdjacentHTML("beforeend", sidebarTemplate);
+
+  // set bounds depending on marker geometry
+  boundsMap(geometry);
+}
+
+// --------------------------------------------------
+// bounds map when sidebar is open
+function boundsMap(geometry) {
+  const sidebar = document.querySelector(".sidebar").offsetWidth;
+
+  const marker = L.marker(geometry);
+  const group = L.featureGroup([marker]);
+
+  // bounds depending on whether we have a marker or not
+  const bounds = geometry ? group.getBounds() : groupBounds.getBounds();
+
+  // set bounds of map depending on sidebar
+  // width and feature group bounds
+  map.fitBounds(bounds, {
+    paddingTopLeft: [geometry ? sidebar : 0, 10],
   });
-  info.update(layer.feature.properties);
 }
-
-// // This resets the highlight after hover moves away
-function resetHighlight(e) {
-  geoJsonLayer.setStyle({
-     color: '#42ff3f', 
-      weight:3, 
-      fillOpacity: 0
-  });
-  info.update();
-}
-
-function zoomToFeature(e) {
-    map.fitBounds(e.target.getBounds());
-}
-
-// This instructs highlight and reset functions on hover movement
-function onEachFeature(feature, layer) {
-  layer.on({
-    mouseover: highlightFeature,
-    //mouseout: highlightFeature,
-    click: highlightFeature,
-    click: zoomToFeature
-  });
-}
-
-
-// Creates an info box on the map
-var info = L.control();
-info.onAdd = function (map) {
-  this._div = L.DomUtil.create('div', 'info');
-  this.update();
-  return this._div;
-};
-
-// Edit info box text and variables (such as elderly density 2014) to match those in your GeoJSON data
-info.update = function (props) {
-  this._div.innerHTML = '<h4>PROGRAMS OF WCS-INDIA<br />PROJECT SITES</h4>' +  (props ?
-    '<b>' + props.Program + ' Program' +'</b><br />' + props.Site + '</b><br />' + props.image +'</b><br />' + props.description + '</b><br />' : 'Click on feature to know more');
-};  
-
-
-info.addTo(map);
