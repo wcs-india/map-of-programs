0623fb334b1a0b8ad2f1a01f2838d9595dbb65a9
monsoonforest <chintz604@gmail.com>
2022-11-04 00:04:38 +0530

    popup-test

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-/* eslint-disable no-undef */
-/**
- * tabs in popup
- */
-
-// config map
-let config = {
-  minZoom: 1,
-  maxZoom: 18,
-};
-// magnification with which the map will start
-const zoom = 5;
-// co-ordinates
-const lat = 20;
-const lng = 79;
-
-// calling map
-const map = L.map("map", config).setView([lat, lng], zoom);
+// Edit the center point and zoom level
+var map = L.map('map', {
+  center: [20,78],
+  zoom: 8,
+  minZoom : 5,
+  scrollWheelZoom: true
+});
 
 // layer controls
 var controlLayers = L.control.layers( null, null, {
@@ -23,43 +12,76 @@ var controlLayers = L.control.layers( null, null, {
      collapsed: false // truw = closed by default
     }).addTo(map);
 
-// Used to load and display tile layers on the map
-// Most tile servers require attribution, which you can set under `Layer`
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+// new L.tileLayer('https://{s}.tile.thunderforest.com/mobile-atlas/{z}/{x}/{y}.png', {
+//   attribution: '&copy; <a href="http://www.thunderforest.com/">Thunderforest</a>, &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//   ,var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+//         attribution: '©OpenStreetMap, ©CartoDB'
+// }).addTo(map);
 
-// custom popup image + text
-const customPopup =
-  '<div class="customPopup"><figure><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg/1920px-A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg"><figcaption>Source: wikipedia.org</figcaption></figure><div>Kraków,[a] also written in English as Krakow and traditionally known as Cracow, is the second-largest and one of the oldest cities in Poland. Situated on the Vistula River in Lesser Poland Voivodeship... <a href="https://en.wikipedia.org/wiki/Krak%C3%B3w" target="_blank">→ show more</a></div></div>';
 
-// specify popup options
-const customOptions = {
-  minWidth: "220", // set max-width
-  keepInView: true, // Set it to true if you want to prevent users from panning the popup off of the screen while it is open.
-};
-function onEachFeature(feature, layer) {
-  layer.bindPopup(customPopup, customOptions)
-  .on("click", clickZoom)
-  .addTo(map);
+// Edit links to your GitHub repo and data source credit
+map.attributionControl.addAttribution('View <a href="https://gitlab.com/monsoonforest/nasa-landslide-nowcast">open-source code on GitLab</a>');
+map.attributionControl.addAttribution('Landslide Nowcast &copy; <a href="https://gpm.nasa.gov/data/visualizations/precip-apps">NASA GPM </a>');
 
-// adding geojson by fetch
-// of course you can use jquery, axios etc.
-fetch("../static/great-indian-bustard-sites.geojson")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    // use geoJSON
-    L.geoJSON(data, {
-      style: {color: '#42ff3f', weight:1, fillOpacity: 0},
-      onEachFeature: onEachFeature
-    }).addTo(map);
+
+new L.esri.basemapLayer('Imagery').addTo(map);
+new L.esri.basemapLayer('ImageryLabels').addTo(map);
+
+$.getJSON("great-indian-bustard-sites.geojson", function (data) {
+  geoJsonLayer = L.geoJson(data, {
+    style: {color: '#42ff3f', weight:1, fillOpacity: 0},
+        onEachFeature: onEachFeature
+  }).addTo(map);
+  controlLayers.addOverlay(geoJsonLayer, '<b>GREAT INDIAN BUSTARD</b>');
+});
+
+// This highlights the layer on hover, also for mobile
+function highlightFeature(e) {
+  resetHighlight(e);
+  var layer = e.target;
+  layer.setStyle({
+    weight: 2,
+    color: '#42ff3f',
+    fillOpacity: 0
   });
-
-
-// center map when click on marker
-function clickZoom(e) {
-  map.setView(e.target.getLatLng(), zoom);
+  info.update(layer.feature.Program);
 }
+
+// This resets the highlight after hover moves away
+function resetHighlight(e) {
+  geoJsonLayer.setStyle(style);
+  info.update();
+}
+
+function zoomToFeature(e) {
+    map.fitBounds(e.target.getBounds());
+}
+
+// This instructs highlight and reset functions on hover movement
+function onEachFeature(feature, layer) {
+  layer.on({
+    mouseover: highlightFeature,
+    mouseout: resetHighlight,
+    click: highlightFeature,
+    click: zoomToFeature
+  });
+}
+
+
+// Creates an info box on the map
+var info = L.control();
+info.onAdd = function (map) {
+  this._div = L.DomUtil.create('div', 'info');
+  this.update();
+  return this._div;
+};
+
+// Edit info box text and variables to match those in your GeoJSON data
+info.update = function (props) {
+  this._div.innerHTML = '<h4>Circle Name <h4>' +  (props ?
+    '<b>' + props.Program + ' ' + '</b><br /><b>' + props.description + '</b><br />' 
+    : 'Click on a Circle');
+};  
+
+
+info.addTo(map);
