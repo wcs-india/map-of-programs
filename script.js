7d9c472445ace3488b56cde0bc0218f7d4047d12
monsoonforest <chintz604@gmail.com>
2022-11-04 02:20:33 +0530

    popup-test

--- script.js
+++ script.js
@@ -18,13 +18,51 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 
+// custom popup image + text
+const customPopup =
+  '<div class="customPopup"><figure><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg/1920px-A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg"><figcaption>Source: wikipedia.org</figcaption></figure><div>Kraków,[a] also written in English as Krakow and traditionally known as Cracow, is the second-largest and one of the oldest cities in Poland. Situated on the Vistula River in Lesser Poland Voivodeship... <a href="https://en.wikipedia.org/wiki/Krak%C3%B3w" target="_blank">→ show more</a></div></div>';
+
+// specify popup options
+const customOptions = {
+  minWidth: "220", // set max-width
+  keepInView: true, // Set it to true if you want to prevent users from panning the popup off of the screen while it is open.
+};
+
+// create marker object, pass custom icon as option, pass content and options to popup, add to map
+L.marker([50.0616, 19.9373], {
+  icon: funny,
+})
+  .bindPopup(customPopup, customOptions)
+  .on("click", clickZoom)
+  .addTo(map);
+
+// center map when click on marker
+function clickZoom(e) {
+  map.setView(e.target.getLatLng(), zoom);
+}
+
 $.getJSON("great-indian-bustard-sites.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
     style: {color: '#42ff3f', weight:1, fillOpacity: 1},
         onEachFeature: onEachFeature
-  }).addTo(map);
-  controlLayers.addOverlay(geoJsonLayer, '<b>GREAT INDIAN BUSTARD</b>');
+  })
+  .bindPopup(customPopup, customOptions)
+  .on("click", clickZoom)
+  .addTo(map);
+  controlLayers.addOverlay(geoJsonLayer, '<b>GREAT INDIAN BUSTARD VILLAGES</b>');
 });
+
+$.getJSON("counter-wildlife-trafficking-sites.geojson", function (data) {
+  geoJsonLayer = L.geoJson(data, {
+    style: {color: '#42ff3f', weight:1, fillOpacity: 1},
+        onEachFeature: onEachFeature
+  })
+.bindPopup(customPopup, customOptions)
+  .on("click", clickZoom)
+  .addTo(map);
+  controlLayers.addOverlay(geoJsonLayer, '<b>COUNTER WILDLIFE TRAFFICKING WORKSHOPS</b>');
+});
+
 
 // This highlights the layer on hover, also for mobile
 function highlightFeature(e) {
@@ -76,48 +114,3 @@ info.update = function (props) {
 
 
 info.addTo(map);
-
-
-
-// adding geojson by fetch
-// of course you can use jquery, axios etc.
-fetch("great-indian-bustard-sites.geojson")
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    let layer = new L.GeoJSON(data, {
-      // A Function that will be called once for each
-      // created Feature, after it has been created and styled
-      onEachFeature: function (feature, layer) {
-        layer.on("mouseover", function (e) {
-          // bindPopup
-          getVoivodeshipName(feature, layer);
-          // show voivodeship
-          addTextToDiv(feature.properties.Program);
-          this.openPopup();
-          // style
-          this.setStyle({
-            fillColor: "#eb4034",
-            weight: 2,
-            color: "#eb4034",
-            fillOpacity: 0.7,
-          });
-        });
-        layer.on("mouseout", function () {
-          this.closePopup();
-          // style
-          this.setStyle({
-            fillColor: "#3388ff",
-            weight: 2,
-            color: "#3388ff",
-            fillOpacity: 0.2,
-          });
-        });
-        layer.on("click", function () {
-          // adding the province name to the visible div
-          addTextToDiv(feature.properties.Program);
-        });
-      },
-    }).addTo(map);
-  });
