8c21aa0617587fe5976d20174bc1910d8c297037
monsoonforest <chintz604@gmail.com>
2022-11-04 13:51:48 +0530

    popups-img-sizes-work

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// Edit the center point and zoom level
-var map = L.map('map', {
-  center: [20,78],
-  zoom: 5,
-  minZoom : 2,
-  scrollWheelZoom: true
-});
+let config = {
+  minZoom: 1,
+  maxZoom: 18,
+};
+// magnification with which the map will start
+const zoom = 6;
+// co-ordinates
+const lat = 22;
+const lng = 77;
 
+// calling map
+const map = L.map("map", config).setView([lat, lng], zoom);
 // layer controls
 var controlLayers = L.control.layers( null, null, {
      position:"topleft",
@@ -17,6 +21,12 @@ L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
+const funny = L.icon({
+  iconUrl: "http://grzegorztomicki.pl/serwisy/pin.png",
+  iconSize: [30, 40], // size of the icon
+  iconAnchor: [20, 58], // changed marker icon position
+  popupAnchor: [0, -60], // changed popup position
+});
 
 
 // specify popup options
@@ -32,14 +42,15 @@ function clickZoom(e) {
 }
 
 
-/////////// GREAT INDIAN BUSTARD////////////////
+//-----------------GREAT INDIAN BUSTARD-------------------------
+
 // custom popup image + text
 const gibPopup =
   '<div class="customPopup"><figure><img src="great-india-bustard.JPG"><figcaption><b>GREAT INDIAN BUSTARD VILLAGES</b></figcaption></figure><div>People from the Mangniyar community from this landscape are recognised for their folk music. We are currently working on outreach activities within the schools of this region where we share  information about Great Indian Bustard and the Orans - traditionally community-conserved areas, through music performances by the Mangniyars. The songs performed during these activities are written by Mangniyars by incorporating information on the ecology of the bird and Orans. <a href="https://india.wcs.org/wildlife/great-indian-bustard" target="_blank">→ show more</a></div></div>';
 
 $.getJSON("great-indian-bustard-sites.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
-    style: {color: '#42ff3f', weight:1, fillOpacity: 1}
+  icon: funny,
         
   })
 .bindPopup(gibPopup, customOptions).on("click", clickZoom)
@@ -47,7 +58,7 @@ $.getJSON("great-indian-bustard-sites.geojson", function (data) {
 controlLayers.addOverlay(geoJsonLayer, '<b>GREAT INDIAN BUSTARD</b>');
 });
 
-//--------------------------------------------------------------------------------/
+//----------------------------COUNTER WILDLIFE TRAFFICKING----------------------------------------------------/
 
 
 const cwtPopup =
@@ -56,15 +67,14 @@ const cwtPopup =
 
 $.getJSON("counter-wildlife-trafficking-sites.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
-    style: {color: '#80032f', weight:1, fillOpacity: 1}
-        
+    
 })
 .bindPopup(cwtPopup, customOptions).on("click", clickZoom)
 .addTo(map);
 controlLayers.addOverlay(geoJsonLayer, '<b>COUNTER WILDLIFE TRAFFICKING</b>');
 });
 
-///---------------------------------------------------------------------------------------
+///-------------------------LIVELIHOOD SUPPORT--------------------------------------------------------------
 
 const livsupPopup =
   '<div class="customPopup"><figure><img src="livelihood-support.jpg"><figcaption><b>LIVELIHOOD SUPPORT PROTECTED AREAS</b></figcaption></figure><div>The major area of focus in support the families relocated under Government Sponsored Relocation Program and intelligence gathering. Support to voluntary relocation program- Livelihood support team provided support to the relocated families  in the field of agriculture, documentation, training etc.<a href="https://india.wcs.org/Programmes/Livelihood-Support2" target="_blank">→ show more</a></div></div>';
@@ -81,7 +91,7 @@ controlLayers.addOverlay(geoJsonLayer, '<b>LIVELIHOOD SUPPORT</b>');
 });
 
 
-///---------------------------------------------------------------------------------------
+///----------------------------------EASTERN GHATS-----------------------------------------------------
 
 const egPopup =
   '<div class="customPopup"><figure><img src="eastern-ghats.JPG"><figcaption><b>EASTERN GHATS PROGRAM</b></figcaption></figure><div>Since 2012, WCS-India has been involved in tiger and prey monitoring in the region, which includes the Nagarjunasagar Srisailam Tiger Reserve (NSTR) in Andhra Pradesh and the Amrabad Tiger Reserve (ATR) in Telangana. Our work in the region has since expanded to human-wildlife conflict mitigation, voluntary relocation, capacity building workshops for law enforcement, legal interventions, community-based conservation interventions, and strengthening protected areas. Our long-term goal is to ensure there are thriving populations of all species in the Nallamala landscape.<a href="https://india.wcs.org/Programmes/Eastern-Ghats-Telangana" target="_blank">→ show more</a></div></div>';
@@ -89,7 +99,7 @@ const egPopup =
 
 $.getJSON("eastern-ghats.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
-    style: {color: '#42fe8c', weight:1, fillOpacity: 1}
+    style: {color: '#40bd81', weight:1, fillOpacity: 1}
         
 })
 .bindPopup(egPopup, customOptions).on("click", clickZoom)
@@ -98,7 +108,7 @@ controlLayers.addOverlay(geoJsonLayer, '<b>EASTERN GHATS</b>');
 });
 
 
-///---------------------------------------------------------------------------------------
+///-----------------------------CARNIVORE & HERBIVORE ECOLOGY & CONSERVATION----------------------------------------------------------
 
 const chePopup =
   '<div class="customPopup"><figure><img src="cwe-photo.JPG"><figcaption><b>CARNIVORE & HERBIVORE ECOLOGY & CONSERVATION</b></figcaption></figure><div>Tigers are a priority species for WCS-India. Our long-term goal for tiger conservation efforts is to support Government efforts to ensure that there are multiple thriving populations of tigers in India. We contribute to the conservation of tigers primarily through the wide variety of field activities we pursue in the Western and Eastern Ghats landscapes, as well as through policy efforts, voluntary relocation, carnivore conflict management, support to enforcement agencies, and capacity building.<a href="https://india.wcs.org/Programmes/Carnivore-Herbivore-Ecology-Conservation" target="_blank">→ show more</a></div></div>';
@@ -114,7 +124,7 @@ $.getJSON("carnivore-herbivore-ecology.geojson", function (data) {
 controlLayers.addOverlay(geoJsonLayer, '<b>CARNIVORE & HERBIVORE ECOLOGY & CONSERVATION</b>');
 });
 
-///---------------------------------------------------------------------------------------
+///----------------------------------MARINE----------------------------------------------------
 
 const marinePopup =
   '<div class="customPopup"><figure><img src="marine.JPG"><figcaption><b>MARINE PROGRAM</b></figcaption></figure><div>The WCS-India Marine Program aims to conserve the unique diversity of India’s coastal habitats and seas by working with communities and partner organizations to overcome these challenges through a multidisciplinary approach. The program’s efforts are focused on five broad themes: (1) strengthening the existing Marine Protected Area (MPA) network (2) mitigating marine megafaunal bycatch (3) strengthening shark and ray management (4) helping incorporate sustainability into marine-centric tourism and (5) promoting and supporting sustainable fisheries.<a href="https://india.wcs.org/Programmes/Marine-program" target="_blank">→ show more</a></div></div>';
@@ -122,7 +132,7 @@ const marinePopup =
 
 $.getJSON("marine-program-sites.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
-    style: {color: '#88aaaa', weight:1, fillOpacity: 1}
+    style: {color: '#2986cc', weight:1, fillOpacity: 1}
         
 })
 .bindPopup(marinePopup, customOptions).on("click", clickZoom)
@@ -130,7 +140,7 @@ $.getJSON("marine-program-sites.geojson", function (data) {
 controlLayers.addOverlay(geoJsonLayer, '<b>MARINE</b>');
 });
 
-///---------------------------------------------------------------------------------------
+///------------------------------------HUMAN WILDLIFE CONFLICT---------------------------------------------------
 
 const hwcPopup =
   '<div class="customPopup"><figure><img src="human-wildlife-conflict.jpeg"><figcaption><b>HUMAN WILDLIFE CONFLICT PROGRAM</b></figcaption></figure><div>Today human-wildlife conflict is an issue that has gained a lot of importance globally and nationally. While it negatively affects both humans and wildlife, many of the latter are endangered and threatened. WCS-India’s work with leopards has been on the forefront of this field of knowledge, with its continuous study of not just the ecology of the wildlife, but also how humans react and respond to the presence of the wildlife in human-use landscapes. <a href="https://india.wcs.org/Programmes/Human-wildlife-interactions" target="_blank">→ show more</a></div></div>';
@@ -146,7 +156,7 @@ $.getJSON("human-wildlife-conflict.geojson", function (data) {
 controlLayers.addOverlay(geoJsonLayer, '<b>HUMAN WILDLIFE CONFLICT</b>');
 });
 
-///---------------------------------------------------------------------------------------
+///-----------------------------------HUMAN WILDLIFE INTERACTIONS----------------------------------------------------
 
 const hwiPopup =
   '<div class="customPopup"><figure><img src="human-wildlife-interactions.JPG"><figcaption><b>HUMAN WILDLIFE INTERACTIONS PROGRAM</b></figcaption></figure><div>Today human-wildlife conflict is an issue that has gained a lot of importance globally and nationally. While it negatively affects both humans and wildlife, many of the latter are endangered and threatened. WCS-India’s work with leopards has been on the forefront of this field of knowledge, with its continuous study of not just the ecology of the wildlife, but also how humans react and respond to the presence of the wildlife in human-use landscapes. <a href="https://india.wcs.org/Programmes/Human-wildlife-interactions" target="_blank">→ show more</a></div></div>';
@@ -154,7 +164,7 @@ const hwiPopup =
 
 $.getJSON("human-wildlife-interactions.geojson", function (data) {
   geoJsonLayer = L.geoJson(data, {
-    style: {color: '#e0b2fa', weight:1, fillOpacity: 1}
+    style: {color: '#e37934', weight:1, fillOpacity: 1}
         
 })
 .bindPopup(hwcPopup, customOptions).on("click", clickZoom)
